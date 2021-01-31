486f8fd8f7c480a5b86338fbf552f737bd59c0b8
Paulo Rabelo <paulorabelo@hotmail.com>
2021-01-31 17:45:29 -0300

    Commit inicial 1a aula

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
 import styled from 'styled-components'
+import db from '../db.json'
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `
 
+const backgroundImage = styled.div`
+  background-image: url(https://i2.wp.com/css-tricks.com/wp-content/uploads/2017/06/css-is-awesome-scaled.jpg?resize=1536%2C1208&ssl=1);
+  flex: 1;
+  background-size: cover;
+  background-position: center;
+`;
+
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <div style={{ backgroundImage: `url(${db.bg})` }}>
+        lolololololol
+    </div>
+  )
 }
